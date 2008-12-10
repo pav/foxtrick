f9db59af16fa824b531b166c4f2f2d08030ff62b
pav <pav@core.(none)>
2008-12-10 23:57:34 +0100

    Added main modules and an example module.

---
+++ content/module.js
@@ -0,0 +1,13 @@
+/**
+ * module.js
+ * @author Mod-PaV
+ * Tools allowing modules to register and listen for events,
+ * such as particular page loads.
+ */
+////////////////////////////////////////////////////////////////////////////////
+Foxtrick.ht_pages = { 'forum' : "\/Forum\/",
+                      'forumViewThread' : '\/Forum\/Read',
+                      'forumWritePost' : '\/Forum\/Write'
+                    };
+////////////////////////////////////////////////////////////////////////////////
+
