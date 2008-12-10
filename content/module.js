0af1d0c6905387046b96064973ade7e16fc21520
pav <pav@core.(none)>
2008-12-11 00:48:43 +0100

    Comments for page names.

--- content/module.js
+++ content/module.js
@@ -5,6 +5,14 @@
  * such as particular page loads.
  */
 ////////////////////////////////////////////////////////////////////////////////
+/** Hattrick pages that modules can run on.
+ * Those values are simply taken from the hattrick URL, so when the current
+ * url contains e.g. "Forum/Read" AND we are on hattrick, all the modules
+ * registered to listen to "forumViewThread" will have their run() functions
+ * called.
+ * You can add new values here, just remember to escape slashes with
+ * backslashes (as you can see below).
+ */
 Foxtrick.ht_pages = { 'forum' : "\/Forum\/",
                       'forumViewThread' : '\/Forum\/Read',
                       'forumWritePost' : '\/Forum\/Write'
